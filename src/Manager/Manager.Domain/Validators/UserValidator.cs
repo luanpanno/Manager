c09573b74a0101b01cc89f556f343e164114e0dd
luanpanno <luanpanno@gmail.com>
2021-09-27 21:41:54 -0300

    feat: domain layer

---
+++ src/Manager/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(80);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(180)
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(6)
+                .MaximumLength(30);
+        }
+    }
+}
